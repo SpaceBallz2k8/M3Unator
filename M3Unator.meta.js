9fa51489e99115be94271f19154e187b50b22903
SpaceBallz2k8 <85353948+SpaceBallz2k8@users.noreply.github.com>
2025-12-15 20:16:46 +0000

    Update M3Unator.meta.js

--- M3Unator.meta.js
+++ M3Unator.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         M3Unator - Web Directory Playlist Creator
 // @namespace    https://github.com/hasanbeder/M3Unator
-// @version      1.0.2
-// @description  Create M3U/M3U8 playlists from directory listing pages. Automatically finds video and audio files in web server indexes.
+// @version      2.0.0
+// @description  Create M3U/M3U8 playlists from directory listing pages. Automatically finds video and audio files in web server indexes. Now with extended M3U and VOD group support.
 // @author       Hasan Beder
 // @license      GPL-3.0
 // @match        *://*/*
@@ -10,8 +10,8 @@
 // @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmNWMyZTciIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48cG9seWdvbiBwb2ludHM9IjIzIDcgMTYgMTIgMjMgMTcgMjMgNyIvPjxyZWN0IHg9IjEiIHk9IjUiIHdpZHRoPSIxNSIgaGVpZ2h0PSIxNCIgcng9IjIiIHJ5PSIyIi8+PC9zdmc+
 // @homepageURL  https://github.com/hasanbeder/M3Unator
 // @supportURL   https://github.com/hasanbeder/M3Unator/issues
-// @downloadURL  https://raw.githubusercontent.com/hasanbeder/M3Unator/main/M3Unator.user.js
-// @updateURL    https://raw.githubusercontent.com/hasanbeder/M3Unator/main/M3Unator.meta.js
 // @run-at       document-end
 // @noframes     true
+// @downloadURL https://update.greasyfork.org/scripts/521593/M3Unator%20-%20Web%20Directory%20Playlist%20Creator.user.js
+// @updateURL https://update.greasyfork.org/scripts/521593/M3Unator%20-%20Web%20Directory%20Playlist%20Creator.meta.js
 // ==/UserScript==
